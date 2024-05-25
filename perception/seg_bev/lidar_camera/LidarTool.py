8a1a8f481eca7ebf20aa82081e2fb25dfb05b983
LivelyPuer <ma15ks35i6boy@gmail.com>
2024-05-25 17:19:26 +0400

    update ipm calibrationTool

--- perception/seg_bev/lidar_camera/LidarTool.py
+++ perception/seg_bev/lidar_camera/LidarTool.py
@@ -2,10 +2,7 @@ import pathlib
 import typing
 
 import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib.colors import Normalize
 from sensor_msgs.msg import PointCloud2
-
 from simulation.webots_ros2_suv.webots_ros2_suv.lib import point_cloud2
 
 
@@ -15,11 +12,11 @@ class LidarTool:
 
     def set_pc2(self, pc: PointCloud2):
         self.points = point_cloud2.read_points(pc)
-        self.points = point_cloud2.read_points(pc)
 
     def set_npy(self, file: typing.Union[str, pathlib.Path]):
         self.points = np.load(file)
-        self.points = self.points[~np.isfinite(self.points)] = 100
+        self.points[~np.isfinite(self.points)] = 100
+        print(self.points)
 
     def get_points(self):
         return self.points
@@ -39,24 +36,65 @@ class LidarTool:
         y[:] += img_size[1] // 2
         y = y.astype(np.int16)
 
-        z = self.points[:, 2]  # Исправлено для использования z в качестве высоты
+        z = self.points[:, 2]
+        z[z == 100] = np.min(z)
+        z_normal, miz, maz = self.normalize_points(z)
+        print(z_normal, miz, maz)
 
-        # Нормализация z для использования в качестве индекса цвета
-        norm = Normalize(vmin=np.min(z), vmax=np.max(z))
-        cmap = plt.get_cmap('viridis')  # Выбор цветовой карты
 
-        # Создание пустого изображения
+
+        # # Создание пустого изображения
         image = np.zeros((img_size[0], img_size[1], 3), dtype=np.uint8)
+        #
+        # # Заполнение изображения
+        for x, y, z in zip(x, y, z_normal):
+            image[img_size[0] - int(y), int(x)] = z * 255
 
-        # Заполнение изображения
-        for i in range(len(x)):
-            color = cmap(norm(z[i]))[:3]  # Получение цвета для текущей высоты
-            color = (np.array(color) * 255).astype(np.uint8)  # Преобразование цвета из [0,1] в [0,255]
-            image[int(y[i]), int(x[i])] = color
+        return image, miz, maz
 
-        return image
 
-    def normalize_points(self, points, scale):
+    def get_YZPlane_colored_by_height(self, scale_factor=10):
+        # # Предполагаем, что point_cloud - это массив Nx3 (N точек, каждая имеет координаты x, y, z)
+        #
+        # # Нормализация x и y для размера изображения
+        img_size = (50 * scale_factor, 50 * scale_factor)  # Размер изображения
+
+        x = self.points[:, 2]
+        max_x = np.max(x[x != 100])
+        x[x == 100] = max_x
+        x[:] *= scale_factor
+        x[:] += img_size[0] // 2
+        x = x.astype(np.int16)
+        y = self.points[:, 1]
+        max_y = np.max(y[y != 100])
+        y[y == 100] = max_y
+        y[:] *= scale_factor
+        y[:] += img_size[1] // 2
+        y = y.astype(np.int16)
+
+        z = self.points[:, 0]
+        z[z == 100] = np.min(z)
+        z_normal, miz, maz = self.normalize_points(z)
+        print(z_normal, miz, maz)
+
+        # # Создание пустого изображения
+        image = np.zeros((img_size[0], img_size[1], 3), dtype=np.uint8)
+        #
+        # # Заполнение изображения
+        for x, y, z in zip(x, y, z_normal):
+            image[img_size[0] - int(y), int(x)] = z * 255
+        # cv2.imshow("XY Point Cloud Image", cv2.rotate(image, cv2.ROTATE_90_COUNTERCLOCKWISE))
+        # cv2.waitKey(0)
+        # cv2.destroyAllWindows()
+        return image, miz, maz
+
+    def normalize_points(self, points):
         min_val = np.min(points)
         max_val = np.max(points)
-        return (points - min_val) / (max_val - min_val) * scale
+        return (points - min_val) / (max_val - min_val), min_val, max_val
+
+    def interpolate_color(self, normalized_z):
+        # Линейная интерполяция между зеленым (0, 255, 0) и красным (255, 0, 0)
+        green = (0, 255, 0)
+        red = (255, 0, 0)
+        return [int((red[i] - green[i]) * normalized_z) for i in range(3)]
--- perception/seg_bev/lidar_camera/main.py
+++ perception/seg_bev/lidar_camera/main.py
@@ -1,8 +1,6 @@
-import cv2
-
-from LidarTool import LidarTool
+from perception.seg_bev.lidar_camera.LidarTool import LidarTool
 
 if __name__ == '__main__':
     lt = LidarTool()
     lt.set_npy("data/20240524-213140.npy")
-    cv2.imshow("title", lt.get_XYPlane_colored_by_height())
+    lt.get_YZPlane_colored_by_height(scale_factor=20)
